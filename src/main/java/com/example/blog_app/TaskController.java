ca40b5e86d853df3e6b6ef664e2b6a561b676aa0
Iwamoto <=2026018@fsc-educ.com>
2026-06-04 14:33:12 +0900

    blogsページのドラフトを作成

---
+++ src/main/java/com/example/blog_app/TaskController.java
@@ -0,0 +1,14 @@
+package com.example.blog_app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class TaskController {
+    @GetMapping("/tasks")
+    public String tasks() {
+        return "tasks";
+    }
+    
+}
